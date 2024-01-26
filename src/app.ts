a35a31c0d57ab94585fb02c0e6ab96d4c8735d60
DanielFariias <danielfariias15@gmail.com>
2024-01-26 14:12:27 -0300

    Add eslint rule and test script, and implement create pet endpoint

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,23 @@
 import fastify from 'fastify'
 import { ZodError } from 'zod'
 import { env } from './env'
+import { makeCreatePetUseCase } from './use-cases/pets/factories/make-create-pet-use-case'
 
 export const app = fastify()
 
 app.get('/', async () => {
   return { hello: 'world' }
+})
+
+app.post('/pets', async (request, reply) => {
+  const createUseCase = makeCreatePetUseCase()
+
+  const { pet } = await createUseCase.execute({
+    name: 'Doug',
+    organizationId: '80bab3b3-4060-4a2c-876d-c272b99ce22d',
+  })
+
+  return reply.status(201).send({ pet })
 })
 
 // Routes
